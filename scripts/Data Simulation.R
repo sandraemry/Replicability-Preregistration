5e43db2453550cc2afb13a30bf1195a52c1c423b
DanieRae <daniellembmacrae@gmail.com>
2020-10-22 20:07:24 -0400

    Just starting off some of the scrip/annotation

--- scripts/Data Simulation.R
+++ scripts/Data Simulation.R
@@ -1,3 +1,12 @@
 #Here we are going to generate some data that reflects the growth of the sea star#
 
-set.seed()
+#Setting the seed to get reproducible random numbers, this vaslue is arbitraty#
+set.seed(2020)
+
+#Next we need to determine the number of observations we will take#
+# use code *n <- VALUE* when we know what this value will be#
+
+#We will want a normal distribution, we will have to extract the mean and std from the paper#
+# x <- rnorm(n = VALUE, mean = u, sd = std)#
+
+  
