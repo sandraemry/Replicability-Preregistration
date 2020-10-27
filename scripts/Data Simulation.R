a0febbfd3fe4635dae8661a0d7b9a6e46d7693b0
DanieRae <daniellembmacrae@gmail.com>
2020-10-27 11:10:53 -0400

    Currenly have reached my limit of knowledge

--- scripts/Data Simulation.R
+++ scripts/Data Simulation.R
@@ -8,21 +8,30 @@
 set.seed(2020)
 
 #Next we need to determine the number of weight(g) observations we will take (sample size, each sea star is weighed as a juvenile)
-n <- 48
+# we might not do this and rather have n defined per line
+#n <- 48
+
+#We want a normal distribution for temperature within a range, or else we change the run to categorical and set the temperatures
+#from 5 -21 degress celcius
+
+x <- runif(n = 24, min = 5, max = 21)
 
 #We will want a normal distribution for the relative growth (change in wet mass/initial mass) this will represent epsilon (e)
-#Current values are for the initial mass of subjects.
+#I'm not fully sure if this is the right way to input e, but without the slope and intercept it is hard to test if this makes sense
+#Current values are for the initial mass of subjects. Calculations for relative growth were not given in the paper
+
 e <- rnorm(n = 48, mean = 4.65, sd = 0.19)
 
-
-#Our temperatures are on the x-axis and will be a pre-determined range (5-21 deg Celsius), not needed to be generated,
-#Currently we have the initial growth measurements going into the 'x' variable, but this can be changed.
-
-# I think we need to be able to apply the linear function from the paper to our initial mass to develop a growth curve#
 
 #defining the slope and then the intercept of the original regression
 
 #b0 <- SLOPE
 
 #b1 <- INTERCEPT
+
+#Next we can try putting it all together and see if we get some data!
+
+#  y = β0 + β1x + ε 
+
+
 
